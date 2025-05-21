5018a5580b2a3cf4acf6608d479d7b01162a3141
marcosjavier1997 <33172949+marcosjavier1997@users.noreply.github.com>
2025-05-21 07:53:15 -0300

    add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useCallback } from "react";
 import { Button } from "./components/ui/button";
 import { Slider } from "./components/ui/slider";
 import logo from './logo.svg';
